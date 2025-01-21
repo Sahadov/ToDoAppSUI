06d0d04f40dcafc3f71a11cdacbe98f66e8adfe2
Dmitry Volkov <dmitrijvolkov@MacBook-Air-Dmitry.local>
2025-01-21 15:38:01 -0300

    Add FireBase

--- ToDoApp/App/ToDoAppApp.swift
+++ ToDoApp/App/ToDoAppApp.swift
@@ -5,10 +5,14 @@
 //  Created by Dmitry Volkov on 21/01/2025.
 //
 
+import FirebaseCore
 import SwiftUI
 
 @main
 struct ToDoAppApp: App {
+    init() {
+        FirebaseApp.configure()
+    }
     var body: some Scene {
         WindowGroup {
             ContentView()

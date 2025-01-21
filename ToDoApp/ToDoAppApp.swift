4f36d0eeaccb520d8f2d05136afd1f59f6bb89be
Dmitry Volkov <dmitrijvolkov@MacBook-Air-Dmitry.local>
2025-01-21 15:24:00 -0300

    Initial commit

---
+++ ToDoApp/ToDoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoAppApp.swift
+//  ToDoApp
+//
+//  Created by Dmitry Volkov on 21/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct ToDoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
